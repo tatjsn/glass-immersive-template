c056139395f458db1ed3fc781857a0d9bf432651
tatjsn <tat_js@yahoo.co.jp>
2014-08-23 21:29:01 +0900

    Add screen dimming

--- src/com/example/testglass/MainActivity.java
+++ src/com/example/testglass/MainActivity.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PowerManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,6 +16,7 @@ import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -83,9 +86,47 @@ public class MainActivity extends Activity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
+		private static final String WAKELOCK_TAG = "tag";
 		private Camera mCamera;
 		private final Handler mHandler = new Handler();
+		private float mSavedScreenBrightness;
 
+		private void dimScreen() {
+			WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
+			mSavedScreenBrightness = lp.screenBrightness;
+			lp.screenBrightness = 0;
+			getActivity().getWindow().setAttributes(lp);
+		}
+
+		private void undimScreen() {
+			WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
+			lp.screenBrightness = mSavedScreenBrightness;
+			getActivity().getWindow().setAttributes(lp);
+		}
+
+		private class ShutterCallback implements Camera.ShutterCallback {
+			@Override
+			public void onShutter() {
+				Log.d("tatdbg", "onShutter");
+				mHandler.postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						Log.d("tatdbg", "goToSleep held=" + mWakeLock.isHeld());
+						dimScreen();
+
+						mHandler.postDelayed(new Runnable() {
+							@Override
+							public void run() {
+								undimScreen();
+								takePicture();
+							}
+						}, 5 * 1000);
+
+					}
+				}, 1 * 1000);
+			}
+
+		}
 		private void takePicture() {
 			Log.d("tatdbg", "take picture");
 			if (mCamera == null) {
@@ -99,19 +140,7 @@ public class MainActivity extends Activity {
 				}
 			}
 			mCamera.startPreview();
-			mCamera.takePicture(
-					new Camera.ShutterCallback() {
-						@Override
-						public void onShutter() {
-							Log.d("tatdbg", "onShutter");
-							mHandler.postDelayed(new Runnable() {
-								@Override
-								public void run() {
-									takePicture();
-								}
-							}, 5 * 1000);
-						}
-					}, null, null,
+			mCamera.takePicture(new ShutterCallback(), null, null,
 					new Camera.PictureCallback() {
 						@Override
 						public void onPictureTaken(byte[] data, Camera camera) {
@@ -120,7 +149,28 @@ public class MainActivity extends Activity {
 					});
 		}
 
+		private PowerManager mPowerManager;
+		private PowerManager.WakeLock mWakeLock;
+
 		public PlaceholderFragment() {
+			setRetainInstance(true);
+		}
+
+		@Override
+		public void onAttach(Activity activity) {
+			super.onAttach(activity);
+			mPowerManager = (PowerManager) activity.getSystemService(Context.POWER_SERVICE);
+			mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, WAKELOCK_TAG);
+			mWakeLock.setReferenceCounted(false);
+			// mWakeLock.acquire(); // No meaning in glass app
+			WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+			mSavedScreenBrightness = lp.screenBrightness;
+		}
+
+		@Override
+		public void onDetach() {
+			super.onDetach();
+			mWakeLock.release();
 		}
 
 		@Override
@@ -128,25 +178,20 @@ public class MainActivity extends Activity {
 				Bundle savedInstanceState) {
 			ViewGroup rootView = (ViewGroup) inflater.inflate(
 					R.layout.fragment_main, container, false);
-//			rootView.setOnClickListener(new View.OnClickListener() {
-//				@Override
-//				public void onClick(View v) {
-//					Log.d("tatdbg", "onclick!!!!");
-//					takePicture();
-//				}
-//			});
+			// Camera wont work without delay
 			mHandler.postDelayed(new Runnable() {
 				@Override
 				public void run() {
 					takePicture();
 				}
-			}, 3 * 1000);
+			}, 1 * 1000);
 
 			return rootView;
 		}
 
 		@Override
 		public void onPause() {
+			Log.d("tatdbg", "pause lock=" + mWakeLock.isHeld());
 			if (mCamera != null) {
 				mCamera.release();
 				mCamera = null;
