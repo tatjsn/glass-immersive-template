8c55e966586fd1f0ddde8cc37ad6a656a427efc6
tatjsn <tat_js@yahoo.co.jp>
2014-09-28 00:46:04 +0900

    Improve camera lifecycle

--- src/com/example/testglass/MainActivity.java
+++ src/com/example/testglass/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
@@ -19,11 +20,40 @@ import com.google.android.glass.sample.timer.SetTimerActivity;
 public class MainActivity extends Activity implements Camera.PictureCallback {
 	private final Handler mHandler = new Handler();
 	private Camera mCamera;
+	private SurfaceHolder mSurfaceHolder;
+	private boolean mSurfaceReady;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mSurfaceHolder = ((SurfaceView) findViewById(R.id.main_surface)).getHolder();
+		mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+			@Override
+			public void surfaceChanged(SurfaceHolder holder, int format,
+					int width, int height) {
+				mSurfaceReady = true;
+				startIntervalShutterIfReady();
+			}
+
+			@Override
+			public void surfaceCreated(SurfaceHolder holder) {
+			}
+
+			@Override
+			public void surfaceDestroyed(SurfaceHolder holder) {
+			}
+		});
+
+		CameraRetriever.get().request(
+				new CameraRetriever.OnResultListener() {
+					@Override
+					public void onResult(Camera result) {
+						mCamera = result;
+						startIntervalShutterIfReady();
+					}
+				});
+
 		findViewById(R.id.main_container).setOnClickListener(
 				new View.OnClickListener() {
 					@Override
@@ -41,28 +71,11 @@ public class MainActivity extends Activity implements Camera.PictureCallback {
 	}
 
 	@Override
-	protected void onResume() {
-		super.onResume();
-		if (mCamera != null) {
-			startIntervalShutter();
-			return;
-		}
-
-		CameraRetriever.get().request(
-				new CameraRetriever.OnResultListener() {
-					@Override
-					public void onResult(Camera result) {
-						mCamera = result;
-						startIntervalShutter();
-					}
-				});
-	}
-
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -100,10 +113,13 @@ public class MainActivity extends Activity implements Camera.PictureCallback {
 		}.execute(data);
 	}
 
-	private void startIntervalShutter() {
-		SurfaceView surface = (SurfaceView) findViewById(R.id.main_surface);
+	private void startIntervalShutterIfReady() {
+		if (!mSurfaceReady || mCamera == null) {
+			return;
+		}
+		Log.d("tatdbg", "start interval");
 		try {
-			mCamera.setPreviewDisplay(surface.getHolder());
+			mCamera.setPreviewDisplay(mSurfaceHolder);
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
 		}
@@ -116,6 +132,7 @@ public class MainActivity extends Activity implements Camera.PictureCallback {
 		mHandler.removeCallbacksAndMessages(null);
 		if (mCamera != null) {
 			mCamera.stopPreview();
+			mCamera.setPreviewCallback(null);
 		}
 	}
 }
